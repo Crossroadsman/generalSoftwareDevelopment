2d14bdc17233fcc5aa2a57c0cb1a9ed43412a17a
CrossroadsMan <crossroadsman@gmail.com>
2015-02-16 12:51:04 -0700

    create apply

---
+++ R/apply.R
@@ -0,0 +1,29 @@
+## apply ##
+
+# What and why does apply exist?
+
+# from: https://nsaunders.wordpress.com/2010/08/20/a-brief-introduction-to-apply-in-r/
+#
+#   At any R Q&A site, you'll frequently see an exchange like this one:
+#
+#     Q: How can I use a loop to [...insert task here...]?
+#     A: Don't. Use one of the apply functions
+
+# Apply functions:
+# apply     Apply functions over array margins
+# by        Apply a function to a data frame split by factors
+# eapply    Apply a function over values in an environment
+# lapply    Apply a function over a list or vector
+# mapply    Apply a function to multiple list or vector arguments
+# rapply    Recursively apply a function over a list
+# sapply    Like lapply but try to simplify the result
+# tapply    Apply a function over a ragged array
+
+# Note: "margin" is just fancy speak for rows or columns or rows and columns
+
+
+
+
+
+
+
