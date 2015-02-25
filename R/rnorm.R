d7e302464a3934c15e08de8042746122f22fdcb8
CrossroadsMan <crossroadsman@gmail.com>
2015-02-25 09:36:43 -0700

    update str and create rnorm

---
+++ R/rnorm.R
@@ -0,0 +1,13 @@
+# Generating random numbers
+
+#rnorm
+#   generate random Normal variates with a given mean and standard deviation
+
+#dnorm
+#   evaluate the Normal probability density (with a given mean/SD) at a point (or vector of points)
+
+#pnorm
+#   evaluate the cumulative distribution function for a Normal distribution
+
+#rpois
+#   generate random Poisson variates with a given rate
--- R/str.R
+++ R/str.R
@@ -1,1 +1,10 @@
 # str() compactly display the internal structure of an R object
+
+str(str)
+str(ls)
+x <- rnorm(100, 2, 4)
+summary(x)
+str(x)
+library(datasets)
+head(airquality)
+str(airquality)
