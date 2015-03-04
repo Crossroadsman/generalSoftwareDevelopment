cf42814a4d93cc0c12e38bf0b0b1d2ea32ad653a
CrossroadsMan <crossroadsman@gmail.com>
2015-03-03 21:52:56 -0700

    update diceroller

--- Python3/diceroller.py
+++ Python3/diceroller.py
@@ -1,5 +1,9 @@
 import random
 
+rseed = random.randint(1, 10000) # in a full implementation this be replaced with a prompt for the user, or the system to pull a true random seed value from random.org
+random.seed(rseed) # seed pseudo random number generator
+
+
 dice = 2
 numberOfSides = 6
 diceTotal = 0
