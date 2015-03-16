2c65f743facd37cdde227532e1cb029a467caff9
CrossroadsMan <crossroadsman@gmail.com>
2015-03-15 21:56:07 -0600

    add beginnigns of creatures file

---
+++ Ruby/creatures.rb
@@ -0,0 +1,20 @@
+class Creatures
+
+@@total_creatures = 0
+@@current_creatures = []
+
+@name = ""
+@description = ""
+@
+
+	def breed()
+		
+	end
+
+	def initialize
+		@@total_creatures += 1
+		@@current_creatures << 
+	
+	end
+
+
