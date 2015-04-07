c2358c913a8086f198149434a759bde722c123ec
Crossroadsman <crossroadsman@gmail.com>
2015-04-07 13:29:54 -0600

    Update sku.rb
    Complete ws from ntb method

--- Ruby/pricing/sku.rb
+++ Ruby/pricing/sku.rb
@@ -3,14 +3,41 @@ class Sku
   @liquid
   @package
   
+  @ws # wholesale
+  @ntb # ntb
+  @ss # sellSheet
+  
+  #Global constants
+  @@gstRate = 0.05
+  
+  #Regional constants, treated as global for time being
+  @@aglcRateBeer = 0.51 # cents per litre
+  @@aglcRateCider = 1.57 # cents per litre
+  @@depositRateKeg = 30.00 # dollars per keg
+  @@depositRateBottle = 0.10 # dollars per bottle
+  @@depositRateCan =  0.10 # dollars per can
+  @@recyclingRateBottle = 0.08 # dollars per bottle
+  @@recyclingRateCan = 0.00 # dollars per can [right now zero, but can and will be non-zero]
+  
+  
+  #Methods
+  #     Initialization
+  
   def initialize(liquid, package)
     @liquid = liquid
     @package = package
   end
   
+  
+  #     Support (e.g., method list)
   def methods
+  
+    methods = {"initialize" => "", "methods" => "", "isBeer" => "", "isCider" => "", "isHighAlcohol" => "" "setPriceWithNtb" => "", "setPiceWithSellsheet" => "", "setPriceWithWholesale" => "", "getNtb" => "", "getSellsheet", "getWholesale"}
+  
   end
   
+  
+  #     Information
   def isBeer
     return @liquid.isBeer == true ? true : false
   
@@ -25,6 +52,25 @@ class Sku
     return @liquid.isHighAlcohol == true ? true : false
   end
   
+  def getNtb
+    return @ntb
+  end
+  
+  def getWholesale
+    return @ws
+  end
+  
+  def getSellsheet
+    return @ss
+  end
+
+
+
+  
+  
+  
+  # Configuration
+  
   def setPriceWithWholesale(wsPrice)
   end
   
@@ -32,17 +78,93 @@ class Sku
   end
   
   def setPriceWithNtb(ntb)
+  
+   # NTB + recycling + AGLC = GST'ble
+    # GST'ble * rate = GST
+    # GST'ble + GST + can deposit + bottle deposit = sell sheet
+    # sell sheet + keg deposit = wholesale
+    
+    #NTB
+    @ntb = ntb
+    
+    
+    #Recycling
+    if @package.isBottle
+    
+        @recyclingRate = @@recyclingRateBottle
+    
+    
+    elsif @package.isCan
+    
+        @recyclingRate = @@recyclingRateCan
+    
+    else
+    
+        @recyclingRate = 0
+    end
+        
+    @recycling = @recyclingRate * @package.items
+    
+    #determine AGLC
+    if @liquid.isBeer
+    
+        @aglcRate = @@aglcRateBeer
+    
+    elsif @liquid.isCider
+    
+        @aglcRate = @@aglcRateCider
+    
+    else
+    
+        return "ERROR: unknown liquid type"
+    
+    end
+    
+    @aglc = @aglcRate * @package.size
+    
+    #GST'ble
+    @gstble = @ntb + @recycling + @aglc
+  
+    #GST
+    @gst = @gstble * @@gstRate
+    
+    #can deposit
+    #bottle desposit
+    if @package.isCan
+    
+        @depositRate = @@depositRateCan
+    
+    elsif @package.isBottle
+    
+        @depositRate = @@depositRateBottle
+    
+    else
+    
+        @depositRate = 0
+    
+    end
+    @packageDeposit = @depositRate * @package.items
+    
+    
+    #sell sheet
+    @ss = @gstble + @gst + @packageDeposit
+    
+    #keg deposit
+    
+    if @package.isDraft
+    
+        @depositRate = 30
+    
+    else
+    
+        @depositRate = 00
+    end
+    @kegDeposit = @depositRate * @@depositRateKeg
+    
+    #wholesale
+    @ws = @ss + @kegDeposit
+  
   end
   
-  def getNtb
-  end
   
-  def getWholesale
-  end
-  
-  def getSellsheet
-  end
-
-
-
 end
